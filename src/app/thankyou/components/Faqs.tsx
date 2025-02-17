b2359f0097625e78e02dd715a811e95fcc8a4fe5
tomas <tomascostapp@gmail.com>
2025-02-17 19:49:00 -0300

    Detalles

--- src/app/thankyou/components/Faqs.tsx
+++ src/app/thankyou/components/Faqs.tsx
@@ -50,7 +50,7 @@ export default function Faqs() {
 								onClick={() => {toggle(item.pregunta)}}
 								className="w-full cursor-pointer p-[20px] border-b border-b-white/50 last:border-b-0"
 							>
-								<h3 className="--sora text-[16px] md:text-[18px] text-white lg:text-[20px] flex justify-between items-center">{item.pregunta}<svg className={`${open === item.pregunta ? 'rotate-45' : ''} min-w-[20px] size-[20px] transition-all duration-300`} fill="#FFF" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></h3>
+								<h3 className="font-semibold text-[20px] text-white flex justify-between items-center">{item.pregunta}<svg className={`${open === item.pregunta ? 'rotate-45' : ''} min-w-[20px] size-[20px] transition-all duration-300`} fill="#FFF" viewBox="0 0 448 512"><path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/></svg></h3>
 								<div className={`normal-case overflow-hidden duration-500 transition-all ${open === item.pregunta  ? 'max-h-[2000px]' : 'max-h-0'}`}>
 									<p className="text-white/80 text-[18px] pt-[10px]">{item.respuesta}</p>
 								</div>
--- src/app/thankyou/page.tsx
+++ src/app/thankyou/page.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import VideoEmbed from "../components/VideoEmbed";
 import Faqs from "./components/Faqs";
 
@@ -14,14 +15,14 @@ export default function Home() {
 				<p className="text-white/80 font-medium text-[16px] md:text-[20px] leading-[100%] pt-[4px]">5.0 Estrellas de 132 reviews</p>
 			</div>
 			<h1 className="text-[26px] leading-[26px] md:text-[50px] font-bold md:leading-[47px] text-center mb-8">
-				¡Ya agendaste tu consulta gratuita! Mira este video para que puedas aprovecharla al maximo.
+				¡Ya agendaste tu entrevista! Mira este video para que puedas aprovecharla al maximo.
 			</h1>
 			<VideoEmbed version={5} />
 		  </div>
 		</section>
 		<section className="relative py-[60px]">
 			<div className="max-w-[900px] mx-auto">
-			<h2 className="text-[32px] md:text-[42px] font-semibold mb-4 text-center max-w-[500px] mx-auto leading-[40px] mb-8">Algunos de los yogis con los que trabajamos:</h2>
+				<h2 className="text-[32px] md:text-[42px] font-semibold mb-8 text-center max-w-[500px] mx-auto leading-[40px]">Algunos de los yogis con los que trabajamos:</h2>
 				<div className="grid grid-cols-2 md:grid-cols-3 gap-4">
 					<iframe className="aspect-[9/16] w-full" src="https://drive.google.com/file/d/1kuByc8o_YLaxpB4J8Zs6eDaWuEe7WM1I/preview"></iframe>
 					<iframe className="aspect-[9/16] w-full" src="https://drive.google.com/file/d/1tKjnRvwPKUHQGRqZFxGElRnQdZtDC_1p/preview"></iframe>
@@ -30,12 +31,34 @@ export default function Home() {
 					<iframe className="aspect-[9/16] w-full" src="https://drive.google.com/file/d/1MgLpATDO0naNk9hjIOr-626265E_CNBx/preview"></iframe>
 					<iframe className="aspect-[9/16] w-full" src="https://drive.google.com/file/d/1pZugQZwfsm6w80ZYadOflFUT8tRzfIL8/preview"></iframe>
 				</div>
+				<h2 className="text-[32px] md:text-[42px] font-semibold mb-8 text-center max-w-[800px] mx-auto leading-[40px] mt-[120px]">Como Julián consiguió 40 nuevos clientes con nuestra estrategia</h2>
+				<iframe className="w-full aspect-video" 
+					src="https://www.youtube.com/embed/SaPx0Z534HI?si=7fyhTfuIWQy-U4XJ&controls=0&modestbranding=1&rel=0&fs=0&disablekb=1&playsinline=1" 
+					title="YouTube video player" 
+					allow="autoplay; encrypted-media">
+				</iframe>
 			</div>
 		</section>
 		<Faqs/>
 		<img className="h-[35px] md:h-[45px] object-contain mx-auto mt-12 mb-8" src="../biofounders.png" alt="Bio Founders" />
 		<p className="text-white/80 font-medium text-[16px] md:text-[20px] text-center mb-[80px]">© Biofounders 2025. Todos los derechos reservados.</p>
+		<Script>
+		{`!function(f,b,e,v,n,t,s)
+		{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+		n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+		if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+		n.queue=[];t=b.createElement(e);t.async=!0;
+		t.src=v;s=b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t,s)}(window, document,'script',
+		'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '942814494324978');
+		fbq('track', 'Lead');`}
+		</Script>
+		<noscript><img height="1" width="1" style={{'display': 'none'}}
+		src="https://www.facebook.com/tr?id=942814494324978&ev=PageView&noscript=1"
+		/></noscript>
 	  </div>
+	  
 	);
   }
   
